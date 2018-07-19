879d3e3e0ff9fbdd22b31cb2b8915a7e9ab1119e
Matt Wong <matthew067d.wong@libertymutual.com>
2018-07-19 12:08:14 -0400

    formatting

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
