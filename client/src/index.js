5545cbb2c3c3d510f2785e43b5352869b289dc20
Christopher Marcenelle <christopher.marcenelle@libertymutual.com>
2018-07-23 09:54:02 -0400

    Update index routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
-import Routes from './routes';
-import configureStore from './store';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import Routes from 'routes';
+import configureStore from 'store';
+import registerServiceWorker from 'registerServiceWorker';
+import 'index.css';
 
 const history = createBrowserHistory();
 const store = configureStore(history);
