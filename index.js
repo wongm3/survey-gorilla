856181f7bff99dbaaa51c470ca06eb515c45724b
Matt Wong <matthew067d.wong@libertymutual.com>
2018-07-23 22:13:07 -0400

    fix priority order for server

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ const app = express();
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.all(['/api/*'], (req, res) => {
+    console.log(process.env.API_HOST + req.url);
+    req.pipe(request(process.env.API_HOST + req.url)).pipe(res);
+});
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
+    console.log('test');
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
-app.all(['/api/*'], (req, res) => {
-    req.pipe(request(process.env.API_HOST + req.url)).pipe(res);
 });
 
 const port = process.env.PORT || 5000;
